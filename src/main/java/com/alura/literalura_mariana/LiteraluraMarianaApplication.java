8a9ddbdfbe0aaae7aa22afed1075b6660e118b30
Lady-Marianne <mariana.andrea.lois@gmail.com>
2024-10-30 16:21:44 -0300

    Iniciar LiterAlura Challenge

---
+++ src/main/java/com/alura/literalura_mariana/LiteraluraMarianaApplication.java
@@ -0,0 +1,13 @@
+package com.alura.literalura_mariana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteraluraMarianaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteraluraMarianaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alura/literalura_mariana/LiteraluraMarianaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.literalura_mariana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteraluraMarianaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
